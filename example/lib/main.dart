c3e371d3489872757dbc22ece6300429593beb1a
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2025-06-22 17:29:38 +0200

    fix: cleanup example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,134 +3,194 @@ import 'package:flutter/material.dart';
 import 'package:pressable/pressable.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pressable',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _defaultTheme = const PressableThemeFill(fillColor: Colors.red);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Example')),
-      body: Column(
-        children: [
-          PressableBuilder(
-            onPressed: () {
-              print('[HomeScreen.build] Builder pressed');
-            },
-            builder: (context, isPressed) {
-              return AnimatedContainer(
-                width: 200,
-                height: 50,
-                color: isPressed ? Colors.blue : const Color(0xFFd6d6d6),
-                duration: const Duration(milliseconds: 100),
-                child: const Center(child: Text('Custom Builder')),
-              );
-            },
-          ),
-          PressableRipple(
-            onPressed: () {
-              print('[HomeScreen.build] Ripple pressed');
-            },
-            child: const ExampleButton(title: 'Ripple'),
-          ),
-          PressableScale(
-            onPressed: () {
-              print('[HomeScreen.build] Scale pressed');
-            },
-            onPressStarted: () {
-              print('[HomeScreen.build] Scale started');
-            },
-            onPressEnded: () {
-              print('[HomeScreen.build] Scale ended');
-            },
-            onPressCanceled: () {
-              print('[HomeScreen.build] Scale canceled');
-            },
-            theme: const PressableThemeScale(scaleFactor: 0.8),
-            child: const ExampleButton(title: 'Scale'),
-          ),
-          PressableOpacity(
-            onPressed: () {
-              print('[HomeScreen.build] Opacity pressed');
-            },
-            onPressStarted: () {
-              print('[HomeScreen.build] Opacity started');
-            },
-            theme: const PressableThemeOpacity(
-              curve: Curves.easeOut,
-              opacityFactor: 0.4,
-            ),
-            child: const ExampleButton(title: 'Opacity'),
-          ),
-          PressableFill(
-            onPressed: () {
-              print('[HomeScreen.build] Fill pressed');
-            },
-            theme: const PressableThemeFill(fillColor: Colors.red),
-            child: const ExampleButton(title: 'Fill'),
-          ),
-          PressablePlatform(
-            onPressed: () {
-              print('[HomeScreen.build] Platform pressed');
-            },
-            ios: const PressableThemeRipple(),
-            android: const PressableThemeRipple(),
-            macos: const PressableThemeRipple(),
-            linux: const PressableThemeRipple(),
-            windows: const PressableThemeRipple(),
-            web: const PressableThemeRipple(),
-            child: const ExampleButton(title: 'Platform'),
-          ),
-          SizedBox(
-            width: 150,
-            child: Stack(
-              children: [
-                PressableScale(
-                  onPressed: () {
-                    print('[HomeScreen.build] Scale pressed');
-                  },
-                  child: Container(
-                    width: 150,
-                    height: 50,
-                    color: Colors.black12,
-                    child: const Center(child: Text('Test')),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.bookmark),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          DefaultPressableTheme(
-            fillTheme: const PressableThemeFill(fillColor: Colors.green),
-            child: PressableFill(
+      appBar: AppBar(
+        title: const Text('Pressable Example'),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            PressableBuilder(
+              key: ValueKey('pressable-builder'),
               onPressed: () {
-                print('[HomeScreen.build] Default theme pressed');
+                print('[HomeScreen.build] Builder pressed');
               },
-              child: const ExampleButton(title: 'Default theme'),
+              builder: (context, isPressed) {
+                return AnimatedContainer(
+                  width: 200,
+                  height: 50,
+                  duration: const Duration(milliseconds: 100),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: isPressed ? Colors.blue : const Color(0xFFd6d6d6),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Custom Builder',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: isPressed ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        ],
+            const SizedBox(height: 8),
+            PressableRipple(
+              key: ValueKey('pressable-ripple'),
+              onPressed: () {
+                print('[HomeScreen.build] Ripple pressed');
+              },
+              child: const ExampleButton(title: 'Ripple'),
+            ),
+            const SizedBox(height: 8),
+            PressableScale(
+              key: ValueKey('pressable-scale'),
+              onPressed: () {
+                print('[HomeScreen.build] Scale pressed');
+              },
+              onPressStarted: () {
+                print('[HomeScreen.build] Scale started');
+              },
+              onPressEnded: () {
+                print('[HomeScreen.build] Scale ended');
+              },
+              onPressCanceled: () {
+                print('[HomeScreen.build] Scale canceled');
+              },
+              theme: const PressableThemeScale(scaleFactor: 0.8),
+              child: const ExampleButton(title: 'Scale'),
+            ),
+            const SizedBox(height: 8),
+            PressableOpacity(
+              key: ValueKey('pressable-opacity'),
+              onPressed: () {
+                print('[HomeScreen.build] Opacity pressed');
+              },
+              onPressStarted: () {
+                print('[HomeScreen.build] Opacity started');
+              },
+              theme: const PressableThemeOpacity(
+                curve: Curves.easeOut,
+                opacityFactor: 0.4,
+              ),
+              child: const ExampleButton(title: 'Opacity'),
+            ),
+            const SizedBox(height: 8),
+            PressableFill(
+              key: ValueKey('pressable-fill'),
+              onPressed: () {
+                print('[HomeScreen.build] Fill pressed');
+              },
+              theme: const PressableThemeFill(fillColor: Colors.red),
+              child: const ExampleButton(title: 'Fill'),
+            ),
+            const SizedBox(height: 8),
+            PressablePlatform(
+              key: ValueKey('pressable-platform'),
+              onPressed: () {
+                print('[HomeScreen.build] Platform pressed');
+              },
+              ios: const PressableThemeRipple(),
+              android: const PressableThemeScale(),
+              macos: const PressableThemeScale(),
+              linux: const PressableThemeFill(),
+              windows: const PressableThemeOpacity(),
+              web: const PressableThemeScale(),
+              child: const ExampleButton(title: 'Platform'),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: 150,
+              child: Stack(
+                children: [
+                  PressableScale(
+                    key: ValueKey('pressable-scale-stack'),
+                    onPressed: () {
+                      print('[HomeScreen.build] Scale pressed');
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 50,
+                      color: Colors.black12,
+                      child: const Center(child: Text('Test')),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.bookmark),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 32),
+            DefaultPressableTheme(
+              fillTheme: _defaultTheme,
+              child: PressableFill(
+                key: ValueKey('pressable-fill-default'),
+                onPressed: () {
+                  print('[HomeScreen.build] Default theme pressed');
+                },
+                child: ExampleButton(
+                  title:
+                      'Default theme ${_defaultTheme.fillColor == Colors.red ? '(red)' : '(blue))'}',
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
+            PressableOpacity(
+              key: ValueKey('pressable-change-default-theme'),
+              child: Text('Change default theme'),
+              onPressed: () {
+                setState(() {
+                  if (_defaultTheme.fillColor == Colors.red) {
+                    _defaultTheme = const PressableThemeFill(
+                      fillColor: Colors.blue,
+                    );
+                  } else {
+                    _defaultTheme = const PressableThemeFill(
+                      fillColor: Colors.red,
+                    );
+                  }
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -144,7 +204,7 @@ class ExampleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
+      width: 225,
       height: 50,
       padding: const EdgeInsets.symmetric(horizontal: 8),
       color: Colors.black12,
--- lib/pressable.dart
+++ lib/pressable.dart
@@ -4,7 +4,7 @@ export 'package:pressable/src/builder.dart';
 export 'package:pressable/src/fill.dart';
 export 'package:pressable/src/opacity.dart';
 export 'package:pressable/src/platform.dart';
-export 'package:pressable/src/provider.dart';
 export 'package:pressable/src/ripple.dart';
 export 'package:pressable/src/scale.dart';
+export 'package:pressable/src/theme/provider.dart';
 export 'package:pressable/src/theme/theme.dart';
